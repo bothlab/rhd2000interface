bd0c88cc17b7f5285069e85f34c64ed6567bc553
Matthias Klumpp <matthias@tenstral.net>
2019-07-17 17:43:16 +0200

    Add CI

---
+++ contrib/ci/cibuild.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+echo "C compiler: $CC"
+echo "C++ compiler: $CXX"
+set -x
+
+#
+# This script is supposed to run inside the PoMiDAQ Docker container
+# on the CI system.
+#
+
+$CC --version
+
+# configure PoMiDAQ build with all flags enabled
+mkdir build && cd build
+qmake \
+    CONFIG+=release \
+    ../source/
+
+# Build, Test & Install
+make -j4
+DESTDIR=/tmp/install_root/ make install
+cd ..
+rm -rf build/
+
+#
+# Build Debian package
+#
+
+#git_commit=$(git log --pretty="format:%h" -n1)
+#git_current_tag=$(git describe --tags 2> /dev/null || echo "v0.1")
+#git_commit_no=$(git rev-list --count HEAD)
+#upstream_version=$(echo "${git_current_tag}" | sed 's/^v\(.\+\)$/\1/;s/[-]/./g')
+#upstream_version="$upstream_version+git$git_commit_no"
+
+#mv contrib/debian .
+#dch --distribution "UNRELEASED"	--newversion="${upstream_version}" -b \
+#    "New automated build from: ${upstream_version} - ${git_commit}"
+
+#dpkg-buildpackage
+#mv ../*.deb .
